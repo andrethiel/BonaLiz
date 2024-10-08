a177d8f35e0bd6e97e063d6f017ac2ab122c13c5
Andre Luiz Thiel <43017985+andrethiel@users.noreply.github.com>
2024-10-07 22:20:44 -0300

    Adicionar arquivos de projeto.

---
+++ BonaLiz.Api/Program.cs
@@ -0,0 +1,30 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+//builder.Services.RegisterServices(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options => // UseSwaggerUI is called only in Development.
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+
+}
+app.UseCors(builder => builder
+            .SetIsOriginAllowed(orign => true)
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials());
+app.MapControllers();
+
+app.Run();
---
+++ BonaLiz.Dados/Context/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using ApiGateway.Dados.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiGateway.Dados.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        //public DbSet<CatalogBrand> CatalogBrands { get; set; }
+        //public DbSet<CatalogType> CatalogTypes { get; set; }
+    }
+
+}
