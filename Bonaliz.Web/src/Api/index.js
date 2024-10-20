a4edada8209caacff81dce709d2ad429a7497965
Andre Luiz Thiel <43017985+andrethiel@users.noreply.github.com>
2024-10-20 19:59:50 -0300

    ajustes novos

--- Bonaliz.Web/src/Api/index.js
+++ Bonaliz.Web/src/Api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://localhost:7166";
+axios.defaults.baseURL = "http://bonaliz.runasp.net/";
 
 const responseBody = (response) => response.data;
 
